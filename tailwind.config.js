6908d94407cb3dc543d72b80c188ec5103f5491b
Oluwashina <shinzbaba@gmail.com>
2023-01-12 22:13:37 +0100

    fonts folder added i.e Lota and Sofia

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        Sofia300: ["Sofia-300"],
+        Sofia400: ["Sofia-400"],
+        Sofia500: ["Sofia-500"],
+        Sofia600: ["Sofia-600"],
+        Lota300: ["Lota-300"],
+        Lota400: ["Lota-400"],
+        Lota600: ["Lota-600"],
+      },
+    },
   },
   plugins: [],
 }
